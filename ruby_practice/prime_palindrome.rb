d7749f6507df826e38b4cd885d6735940620b60f
Richard Lau <richardlau@Richards-MacBook-Pro.local>
2024-10-20 20:28:31 -0700

    add ruby practice

---
+++ ruby_practice/prime_palindrome.rb
@@ -0,0 +1,23 @@
+# Task
+
+# Your task is to print an array of the first  palindromic prime numbers.
+# For example, the first  palindromic prime numbers are .
+
+# Input Format
+
+# A single line of input containing the integer .
+
+# Constraints
+
+# You are not given how big  is.
+
+# Output Format
+
+# Print an array of the first  palindromic primes.
+
+# Sample Input
+
+# 5
+# Sample Output
+
+# [2, 3, 5, 7, 11]
